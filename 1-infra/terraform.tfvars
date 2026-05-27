b175e6eb3b5fa935ef80c02352eafac4d8c3e578
andreluiz1035 <andreluiz.mcse@gmail.com>
2026-05-27 12:18:10 -0300

    27051

---
+++ 1-infra/terraform.tfvars
@@ -0,0 +1,2 @@
+subscription_id = "e215d238-0040-4427-8579-a803f05ea1c3"
+admin_password  = "$1Vennecy$1"
---
+++ 1-infra/variables.tf
@@ -0,0 +1,23 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "Brazil South"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "rg-netbird-lab"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "vennecy"
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
